dc29c1d6154d0cef0b3bb3718e5abb403e2aa692
Abola Lee <abola921@gmail.com>
2020-06-01 16:54:42 +0800

    2020 修訂版本，增加參數功能

--- sample/hello/app.py
+++ sample/hello/app.py
@@ -1,9 +1,19 @@
 from flask import Flask
+from argparse import ArgumentParser
+
 app = Flask(__name__)
+parser = ArgumentParser()
+
+parser.add_argument("--show-words", help="自定義顯示文字", dest="showWords", default="Hello World!!!")
+
+showWords = ""
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!!!'
+def hello():
+    return showWords
 
 if __name__ == '__main__':
+    args = parser.parse_args()
+    showWords = args.showWords
+
     app.run(debug=True,host='0.0.0.0')
